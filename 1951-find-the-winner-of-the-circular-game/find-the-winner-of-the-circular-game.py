f58eb1900eb3beb77c8ee774ee2501465beaea48
Adarsh Siddharth <adarshsiddh23@gmail.com>
2026-08-17 14:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (59.60%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,7 +1,7 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         j=0
-        for i in range(2,n+1):
+        for i in range(1,n+1):
             j=(j+k)%i
         return j+1
 
