d428614f595c25342a05d66347774bc979c7f8de
Adarsh Siddharth <adarshsiddh23@gmail.com>
2026-08-31 00:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (10.75%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.c
@@ -0,0 +1,20 @@
+int maxArea(int* height, int heightSize) {
+    int left = 0, right = heightSize - 1;
+    int maxArea = 0;
+
+    while (left < right) {
+        int h = (height[left] < height[right]) ? height[left] : height[right];
+        int width = right - left;
+        int area = h * width;
+
+        if (area > maxArea)
+            maxArea = area;
+
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+
+    return maxArea;
+}
